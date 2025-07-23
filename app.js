900d9bd14e1f397481dba9eaa70587d841cd36d4
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-07-24 01:26:26 +0300

    add load data and render

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ function mainUserListApp() {
       container: 'container',
       title: 'title',
       userList: 'user-list',
+      userCard: 'user-card',
+      cardHeader: 'card-header',
+      cardBody: 'card-body',
+      cardMainInfo: 'card-main-info',
+      cardAdress: 'card-address',
     };
 
     const selectors = {
@@ -29,6 +34,11 @@ function mainUserListApp() {
       container: `.${classes.container}`,
       title: `.${classes.title}`,
       userList: `.${classes.userList}`,
+      userCard: `${classes.userCard}`,
+      cardHeader: `${classes.cardHeader}`,
+      cardBody: `${classes.cardBody},`,
+      cardMainInfo: `${classes.cardMainInfo}`,
+      cardAdress: `${classes.cardAdress}`,
       appendLocation: '.ins-api-users',
     };
 
@@ -42,7 +52,7 @@ function mainUserListApp() {
       self.reset();
       self.buildCSS();
       self.buildHTML();
-      self.fetchUsers();
+      self.checkAndLoadData();
       self.setEvents();
     };
 
@@ -63,7 +73,6 @@ function mainUserListApp() {
       const html = `
         <div class="${classes.container}">
             <h1 class="${classes.title}">User List</h1>
-            <div class="${classes.userList}"></div>
         </div>
       `;
       $(selectors.appendLocation).prepend(html);
@@ -71,21 +80,65 @@ function mainUserListApp() {
 
     self.setEvents = () => {};
 
-    //api fetch
-    self.fetchUsers = () => {
-      fetch(API_URL)
-        .then((respond) => {
-          if (!respond.ok) throw new Error('API respond failed');
-          return respond.json();
-        })
-        .then((data) => {
-          self.saveToStorage(data);
-        })
-        .catch((err) => {
-          console.error('api error: ', err);
-        });
+    //localde varsa getir. yoksa apiden çek
+    self.checkAndLoadData = () => {
+      let users = self.getFromStorage();
+      if (users) {
+        self.renderList(users);
+      } else {
+        //api fetch
+        fetch(API_URL)
+          .then((respond) => {
+            //bunu ekledim çünkü daha önce ağzım yandı bundan
+            //fetch sadece network hatalarında reject ediyor. bu yüzden sunucudan 4xx, 5xx gibi yanıtlar dönerse burda yakalıyorum
+            if (!respond.ok) throw new Error('API respond failed');
+            return respond.json();
+          })
+          .then((data) => {
+            self.saveToStorage(data);
+            self.renderList(data);
+          })
+          .catch((err) => {
+            console.error('api error: ', err);
+          });
+      }
     };
 
+    //parametre olarak gelen user datasıyla card oluştur
+    self.renderList = (users) => {
+      const $container = $(selectors.container);
+      const $userList = $(`<div class="${classes.userList}"></div>`);
+
+      $.each(users, function (index, user) {
+        const $card = $(`<div class=${classes.userCard}></div>`);
+
+        const $cardHeader = $(`<div class=${classes.cardHeader}></div>`);
+        const $h2 = $(`<h2>${user.name}</h2>`);
+        $cardHeader.append($h2);
+        $card.append($cardHeader);
+
+        const $cardBody = $(`<div class=${classes.cardBody}>`);
+
+        const $cardMainInfo = $(`<div class=${classes.cardMainInfo}>`);
+        $cardMainInfo.append(`<p>${user.username}</p>`);
+        $cardMainInfo.append(`<p>${user.email}</p>`);
+        $cardBody.append($cardMainInfo);
+
+        const $cardAddress = $(`<div class=${classes.cardAdress}>`);
+        $cardAddress.append(`<p>${user.address.city}</p>`);
+        $cardAddress.append(`<p>${user.address.street}</p>`);
+        $cardAddress.append(`<p>${user.address.suite}</p>`);
+        $cardAddress.append(`<p>${user.address.zipcode}</p>`);
+        $cardBody.append($cardAddress);
+
+        $card.append($cardBody);
+        $userList.append($card);
+      });
+
+      $container.append($userList);
+    };
+
+    //LOCALSTORAGE İŞLEMLERİ
     //locale kaydetme
     self.saveToStorage = (data) => {
       const toStore = {
@@ -93,6 +146,27 @@ function mainUserListApp() {
         exp: Date.now(),
       };
       localStorage.setItem(STORAGE_KEY, JSON.stringify(toStore));
+    };
+
+    //localden çekme
+    self.getFromStorage = () => {
+      const stored = localStorage.getItem(STORAGE_KEY);
+      if (!stored) return null;
+
+      try {
+        const parsed = JSON.parse(stored);
+
+        if (!parsed.exp || !parsed.users) return null;
+
+        if (Date.now() - parsed.exp > CACHE_DURATION) {
+          localStorage.removeItem(STORAGE_KEY);
+          return null;
+        }
+
+        return parsed.users;
+      } catch {
+        return null;
+      }
     };
 
     $(document).ready(self.init);
